f456c296b6ea4a8eefc5ce67b2c870cd8dfa8b57
Dollar Double Empire <31463815+BermudaLocals@users.noreply.github.com>
2026-08-04 01:10:16 -0300

    Bump SW cache version to v3 to bust old creator.html cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='nvme-v2';
+const CACHE_NAME='nvme-v3';
 const urlsToCache=['/','/app','/creator','/public/app.html','/public/creator.html','/public/live.html','/img/logo.png','/img/default-avatar.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(n=>Promise.all(n.filter(n=>n!==CACHE_NAME).map(n=>caches.delete(n)))).then(()=>self.clients.claim()))});
